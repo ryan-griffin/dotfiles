86e91dde305055d0284184a769961cbdaf0a4f94
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-01-26 19:08:14 -0500

    more deps

--- deps.sh
+++ deps.sh
@@ -1,1 +1,1 @@
-yay -S exa bat hyprland hyprpaper-git kitty rofi-lbonn-wayland waybar-hyprland dunst inter-font ttf-jetbrains-mono-nerd
+yay -S exa bat hyprland hyprpaper-git kitty rofi-lbonn-wayland waybar-hyprland dunst inter-font ttf-jetbrains-mono-nerd zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting
