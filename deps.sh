57970763a20995f8e8bd31db72bde52eb51582d0
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-01-29 15:44:33 -0500

    change to paru

--- deps.sh
+++ deps.sh
@@ -1,1 +1,1 @@
-yay -S zsh zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting exa bat inter-font ttf-jetbrains-mono-nerd hyprland hyprpaper-git kitty rofi-lbonn-wayland waybar-hyprland mako
+paru -S zsh zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting exa bat inter-font ttf-jetbrains-mono-nerd hyprland hyprpaper-git waybar-hyprland rofi-lbonn-wayland mako playerctl kitty
