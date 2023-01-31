921bc3fa23c394478f50e305c15aaa6acfa7f6fb
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-01-30 19:19:57 -0500

    screenshot

--- deps.sh
+++ deps.sh
@@ -1,1 +1,1 @@
-paru -S zsh zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting exa bat inter-font ttf-jetbrains-mono-nerd hyprland hyprpaper-git waybar-hyprland rofi-lbonn-wayland mako playerctl kitty
+paru -S zsh zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting exa bat inter-font ttf-jetbrains-mono-nerd hyprland hyprpaper-git waybar-hyprland rofi-lbonn-wayland mako grimblast-git playerctl kitty
