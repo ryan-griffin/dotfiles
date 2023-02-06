6dd8f33e7af38e9e1c722383f15c98d92b4b6e05
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-02-06 18:33:27 -0500

    more deps

--- deps.sh
+++
@@ -1,1 +0,0 @@
-paru -S zsh zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting exa bat inter-font ttf-jetbrains-mono-nerd hyprland hyprpaper-git waybar-hyprland rofi-lbonn-wayland mako grimblast-git playerctl kitty spicetify-cli
---
+++ pkgs.sh
@@ -0,0 +1,1 @@
+paru -S zsh zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting exa bat inter-font ttf-jetbrains-mono-nerd hyprland xdg-desktop-portal-hyprland-git hyprpaper-git waybar-hyprland rofi-lbonn-wayland mako grimblast-git pipewire-pulse playerctl kitty webcord spotify spicetify-cli visual-studio-code-bin gnome-keyring
