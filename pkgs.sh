d9a13c19f1a287f9f79aaf9c9d36559d2a192b8e
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-02-12 21:39:33 -0500

    pkgs.sh redo

--- pkgs.sh
+++ pkgs.sh
@@ -1,1 +1,33 @@
-paru -S zsh zsh-autocomplete-git zsh-autosuggestions zsh-syntax-highlighting starship exa bat ripgrep inter-font ttf-jetbrains-mono-nerd ttf-apple-emoji hyprland xdg-desktop-portal-hyprland-git hyprpaper-git waybar-hyprland rofi-lbonn-wayland mako grimblast-git pipewire-pulse playerctl kitty webcord spotify spicetify-cli visual-studio-code-bin gnome-keyring
+packages=(
+	"zsh"
+	"zsh-autocomplete-git"
+	"zsh-autosuggestions"
+	"zsh-syntax-highlighting"
+	"starship"
+	"exa"
+	"bat"
+	"ripgrep"
+	"inter-font"
+	"ttf-jetbrains-mono-nerd"
+	"ttf-apple-emoji"
+	"hyprland"
+	"xdg-desktop-portal-hyprland-git"
+	"hyprpaper-git"
+	"waybar-hyprland"
+	"rofi-lbonn-wayland"
+	"mako"
+	"grimblast-git"
+	"pipewire-pulse"
+	"playerctl"
+	"kitty"
+	"firefox"
+	"webcord"
+	"spotify"
+	"spicetify-cli"
+	"visual-studio-code-bin"
+	"gnome-keyring"
+)
+
+for package in "${packages[@]}"; do
+	paru -S --noconfirm "$package"
+done
