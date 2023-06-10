673fdf904abe4c4414206b2250db8195d66c8bc7
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-06-10 18:55:30 -0400

    gsettings.sh

---
+++ gsettings.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.desktop.interface gtk-theme dark-decay
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface font-name "Sans Regular 11"
+gsettings set org.gnome.desktop.interface font-hinting full
+gsettings set org.gnome.desktop.interface font-antialiasing rgba
