0088121c0d0be549797e1d3f6c610db5922bf700
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-06-17 15:43:32 -0400

    treesitter

--- ryang/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ ryang/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,4 +3,18 @@ return {
 	build = function()
 		require("nvim-treesitter.install").update({ with_sync = true })
 	end,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true
+			},
+			indent = {
+				enable = true
+			},
+		})
+	end,
 }
