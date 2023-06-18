1877748146dd1fbe6c55f53e99924bfcaf49f273
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-06-18 13:03:50 -0400

    fix

--- ryang/.config/nvim/lua/plugins/decay.lua
+++ ryang/.config/nvim/lua/plugins/decay.lua
@@ -1,6 +1,5 @@
 return {
 	"decaycs/decay.nvim",
-	lazy = false,
 	priority = 1000,
 	config = function()
 		require("decay").setup({
--- ryang/.config/nvim/lua/plugins/nvim-tree.lua
+++ ryang/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,7 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	version = "*",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		require("nvim-tree").setup()
 	end,
