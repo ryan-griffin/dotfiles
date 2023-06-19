f2f78aefec398e7e29b294efdb4340a3a720c3a8
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-06-19 14:50:08 -0400

    cmp update

--- ryang/.config/nvim/lua/plugins/nvim-cmp.lua
+++ ryang/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -8,7 +8,8 @@ return {
 		"saadparwaiz1/cmp_luasnip",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline"
+		"hrsh7th/cmp-cmdline",
+		"onsails/lspkind.nvim",
 	},
 	config = function()
 		-- Set up nvim-cmp.
@@ -23,13 +24,29 @@ return {
 				end,
 			},
 
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					local kind = require("lspkind").cmp_format({
+						mode = "symbol_text",
+						maxwidth = 50,
+					})(entry, vim_item)
+					local strings = vim.split(kind.kind, "%s", { trimempty = true })
+					kind.kind = (strings[1] or "") .. " "
+					kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+					return kind
+				end
+			},
+
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
+
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- For luasnip users.
