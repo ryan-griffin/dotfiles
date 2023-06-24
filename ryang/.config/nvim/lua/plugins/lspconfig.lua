93e3ec9849f9fd0d0b63104f5eac2454d34199ba
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-06-24 17:52:48 -0400

    format on save

--- ryang/.config/nvim/lua/plugins/lspconfig.lua
+++ ryang/.config/nvim/lua/plugins/lspconfig.lua
@@ -118,5 +118,12 @@ return {
 		require("mason-null-ls").setup({
 			handlers = {}
 		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = buffer,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end
+		})
 	end,
 }
