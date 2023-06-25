9753f664259413402f99f422b8959f809e400973
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-06-25 16:40:53 -0400

    indent blankline update

--- ryang/.config/nvim/lua/plugins/indent-blankline.lua
+++ ryang/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,1 +1,8 @@
-return { "lukas-reineke/indent-blankline.nvim" }
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	opts = {
+		use_treesitter = true,
+		show_current_context = true
+	}
+}
