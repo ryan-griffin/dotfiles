d7e16142407909d949524defdcc7065a992db947
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-07-12 19:44:54 -0400

    auto format improvement

--- ryang/.config/nvim/lua/plugins/lspconfig.lua
+++ ryang/.config/nvim/lua/plugins/lspconfig.lua
@@ -124,23 +124,28 @@ return {
 		require("mason").setup()
 		require("mason-lspconfig").setup()
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local augroup = vim.api.nvim_create_augroup("LspCapabilities", {})
 		require("mason-lspconfig").setup_handlers({
 			function(server_name)
 				require("lspconfig")[server_name].setup({
-					capabilities = capabilities
+					capabilities = capabilities,
+					on_attach = function(client, buffer)
+						if client.supports_method("textDocument/formatting") then
+							vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePre" }, {
+								group = augroup,
+								buffer = buffer,
+								callback = function()
+									vim.lsp.buf.format({ async = false })
+								end
+							})
+						end
+					end
 				})
-			end,
+			end
 		})
 
 		require("mason-null-ls").setup({
 			handlers = {}
-		})
-
-		vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePre" }, {
-			buffer = buffer,
-			callback = function()
-				vim.lsp.buf.format({ async = false })
-			end
 		})
 	end
 }
