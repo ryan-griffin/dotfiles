57b8b370fe4ae2523051282f4e1876f981527bf9
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2024-11-16 19:02:34 -0500

    gsettings font tweaks

--- gsettings.sh
+++ gsettings.sh
@@ -1,7 +1,5 @@
 gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-gsettings set org.gnome.desktop.interface font-name "Sans Regular 11"
-gsettings set org.gnome.desktop.interface document-font-name "Sans Regular 11"
+gsettings set org.gnome.desktop.interface font-name "Sans Regular 10.5"
+gsettings set org.gnome.desktop.interface document-font-name "Sans Regular 10.5"
 gsettings set org.gnome.desktop.interface monospace-font-name "Monospace Regular 11"
-gsettings set org.gnome.desktop.interface font-hinting full
-gsettings set org.gnome.desktop.interface font-antialiasing rgba
