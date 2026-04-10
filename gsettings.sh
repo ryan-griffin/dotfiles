bd29a595d6b26a6c4539f58e816f262d7df4efcd
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2026-04-10 18:02:30 -0400

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -1,5 +1,6 @@
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-gsettings set org.gnome.desktop.interface font-name "Sans Regular 10.5"
-gsettings set org.gnome.desktop.interface document-font-name "Sans Regular 10.5"
-gsettings set org.gnome.desktop.interface monospace-font-name "Monospace Regular 11"
+gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
+gsettings set org.gnome.desktop.interface font-name "Adwaita Sans 11"
+gsettings set org.gnome.desktop.interface document-font-name "Adwaita Sans 11"
+gsettings set org.gnome.desktop.interface monospace-font-name "JetBrainsMono Nerd Font 11"
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile "flat"
